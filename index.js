4b4f58203ede562dccdb83b687fe324c2a217104
Shane Tang <emperor.stang@gmail.com>
2020-04-17 03:18:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const path = require('path')
+const app = new express();
+const path = require('path');
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.listen(PORT);
+console.log("listening on port " + PORT);
