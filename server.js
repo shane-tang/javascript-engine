b5fc5ade57ab934a74916055dc3ab93fbb0a8bf2
Shane Tang <emperor.stang@gmail.com>
2020-04-19 01:11:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,13 @@
-// const express = require('express');
-// const app = new express();
-// const path = require('path');
-
-// app.use('/', express.static(__dirname));
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname+'/dist/index.html'));
-// });
-
-// app.listen(PORT);
-// console.log("listening on port " + PORT);
-
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = new express();
 const PORT = process.env.PORT || 5000;
+const path = require('path');
 
-fs.readFile('./dist/index.html', (err, html) => {
-    http.createServer((req, res) => {
-        res.write(html);
-        res.end();
-    }).listen(PORT);
+app.use(express.static(__dirname + '/dist/'));
 
-    console.log("listening on port " + PORT);
-
+app.get('/', (req, res) => {
+  res.sendFile('/index.html');
 });
+
+app.listen(PORT);
+console.log("listening on port " + PORT);
