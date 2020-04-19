4bb508b1ee8c71c6e7d2a656631a358083920bbe
Shane Tang <emperor.stang@gmail.com>
2020-04-19 19:24:02 -0400

    scaling update

--- src/canvas.js
+++ src/canvas.js
@@ -2,47 +2,50 @@ module.exports = {
     generateCanvas: function generateCanvas(gameWidth, gameHeight) {
         var canvas = document.createElement('canvas');
         canvas.id = "game";
-        var context = canvas.getContext('2d');
-
-        var ratio = window.devicePixelRatio || 1;
-
-
-
-        var windowWidth = window.innerWidth;
-        var windowHeight = window.innerHeight;
-        var windowRatio = windowWidth / windowHeight;
-
-        var gameRatio = gameWidth / gameHeight;
 
         canvas.width = gameWidth;
         canvas.height = gameHeight;
+        canvas.style.width = gameWidth
+        canvas.style.height = gameHeight;
 
-
-        // wide to narrow
-        if (windowRatio > gameRatio) {
-            // scale via width
-            var scaleFactor = gameRatio / windowRatio;
-            canvas.style.width = windowWidth * scaleFactor + 'px';
-            canvas.style.height = windowHeight + 'px';
-        }
-        else {
-        // if (windowRatio < gameRatio) {
-            var scaleFactor = windowRatio / gameRatio;
-            canvas.style.width = windowWidth + 'px';
-            canvas.style.height = windowHeight * scaleFactor + 'px';
-        }
-
-
-        // canvas.width = Math.round(gameWidth * ratio);
-        // canvas.height = Math.round(gameHeight * ratio);
-        // canvas.style.width = windowWidth + 'px';
-        // canvas.style.height = windowHeight + 'px';
-
-        // context.scale(scaleFactor, scaleFactor);
-
-        // context.setTransform(ratio, 0, 0, ratio, 0, 0)
-        canvas.style.border = '2px solid blue';
+        var context = canvas.getContext('2d');
+        context.beginPath();
+        context.rect(0, 0, 1080, 1920);
+        context.stroke();
 
         return canvas
     }
 }
+
+// module.exports = {
+//     generateCanvas: function generateCanvas(gameWidth, gameHeight) {
+//         var canvas = document.createElement('canvas');
+//         canvas.id = "game";
+//         // var context = canvas.getContext('2d');
+
+//         // var ratio = window.devicePixelRatio || 1;
+
+//         var windowWidth = window.innerWidth;
+//         var windowHeight = window.innerHeight;
+//         var windowRatio = windowWidth / windowHeight;
+
+//         var gameRatio = gameWidth / gameHeight;
+
+//         canvas.width = gameWidth;
+//         canvas.height = gameHeight;
+
+//         if (windowRatio > gameRatio) {
+//             window.scaleFactor = gameRatio / windowRatio;
+//             canvas.style.width = windowWidth * window.scaleFactor + 'px';
+//             canvas.style.height = windowHeight + 'px';
+//         } else {
+//             window.scaleFactor = windowRatio / gameRatio;
+//             canvas.style.width = windowWidth + 'px';
+//             canvas.style.height = windowHeight * window.scaleFactor + 'px';
+//         }
+
+//         canvas.style.border = '2px solid blue';
+
+//         return canvas
+//     }
+// }
--- src/game.js
+++ src/game.js
@@ -1,5 +1,4 @@
 const canvasUtils = require('./canvas.js');
-const gameContainer = $('#gameContainer');
 
 const width = 1080;
 const height = 1920;
@@ -8,20 +7,24 @@ var game = function game() {
     this.viewport = canvasUtils.generateCanvas(width, height);
     this.context = viewport.getContext('2d');
 
-    gameContainer.append(this.viewport);
+    $('body').append(this.viewport);
 
     var drawing = new Image();
     drawing.src = "img.jpg";
     drawing.onload = () => {
-        this.context.drawImage(drawing,0,0);
+        drawing.width = drawing.width;
+        drawing.height = drawing.height;
+        context.drawImage(drawing,0,0);
     }
     
     this.context.font = '32px Arial';
-    this.context.fillText(window.devicePixelRatio, 5, 50, 800);
+    this.context.fillText(window.devicePixelRatio, 5, 500);
 
     return this;
 }
 
+window.gameWidth = width;
+window.gameHeight = height;
 window.viewport = game();
 
 module.exports = game;
