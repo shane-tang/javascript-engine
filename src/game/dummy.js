1ec69f4718b68f26316f1d6dc3b8bde9fb23de8d
Shane Tang <emperor.stang@gmail.com>
2020-05-04 01:44:41 -0400

    Create dummy.js

---
+++ src/game/dummy.js
@@ -0,0 +1,46 @@
+const imagePath = require('./patrick.jpg').default;
+const ImageEntity = require('../entities/image');
+var GAME;
+var patrick;
+var velocity = 5;
+
+function init() {
+    GAME = window.game;
+
+    patrick = new ImageEntity({
+        id: "test image",
+        filepath: imagePath
+    });
+
+    GAME.pushEntity(patrick);
+
+    patrick.onKeyPress('s', () => {
+        patrick.yPos += 5;
+    });
+
+    patrick.onKeyPress('w', () => {
+        patrick.yPos -= 5;
+    });
+}
+
+function onStart() {
+
+}
+
+function eachTick(entities) {
+    patrick.xPos += velocity;
+    if (patrick.xPos + patrick.width > GAME.width || patrick.xPos < 0) {
+        velocity *= -1;
+        patrick.xPos += (velocity * 5);
+    }
+}
+
+function moveRight(amount) {
+    patrick.xPos += amount;
+}
+
+module.exports = {
+    init: init,
+    onStart: onStart,
+    eachTick: eachTick
+};
